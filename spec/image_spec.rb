05b20c4271ab619323f92e6bab6db1399a0bac1d
Benjamin Eckel <benjamin.eckel@gmail.com>
2011-12-09 22:51:32 -0600

    trying a jpg test image

--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -3,10 +3,9 @@ require 'spec_helper'
 describe CloseEnough::Ocr do
 
   it "should find a location from an image" do
-    CloseEnough::Ocr.load_locations! ["Igor's", "Half Moon", "The Saint", "Avenue Pub", "Lucky's", "City Hall"]
-    puts CloseEnough::Ocr.locations_from_image(File.join(File.dirname(__FILE__), 'event.gif'))
+    CloseEnough::Ocr.load_locations! ["Igor's", "Half Moon", "The Saint", "Avenue Pub", "Lucky's", "City Hall", "American University"]
+    puts CloseEnough::Ocr.locations_from_image(File.join(File.dirname(__FILE__), 'event.jpg'))
   end
-
 
 end
 
